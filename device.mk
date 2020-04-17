b62f8b558ab1a776284b1b49533ea433df0eed79
Thagoo <lohitgowda56@gmail.com>
2020-04-17 06:03:52 +0000

    rolex: Drop custom offline charger binary
    * And build aosp charger images
    
    Signed-off-by: Thagoo <lohitgowda56@gmail.com>

--- device.mk
+++ device.mk
@@ -114,6 +114,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
 # CNE
 PRODUCT_PACKAGES += \
     libcnefeatureconfig
