koito97/android_device_xiaomi_rolex

rolex: Replace KeyDisabler class with Touch HAL
Change-Id: I7b87ae0ad834ba02a78696afe393d9d4f8920fbd
Signed-off-by: SunnyRaj84348 <sunnyraj7461@gmail.com>

touch/KeyDisabler.cpp
@@ -1,0 +1,64 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <android-base/strings.h>
+
+#include "KeyDisabler.h"
+
+namespace vendor {
+namespace lineage {
+namespace touch {
+namespace V1_0 {
+namespace implementation {
+
+constexpr const char kControlPath[] = "/proc/touchpanel/capacitive_keys_enable";
+
+KeyDisabler::KeyDisabler() {
+    mHasKeyDisabler = !access(kControlPath, F_OK);
+}
+
+// Methods from ::vendor::lineage::touch::V1_0::IKeyDisabler follow.
+Return<bool> KeyDisabler::isEnabled() {
+    std::string buf;
+
+    if (!mHasKeyDisabler) return false;
+
+    if (!android::base::ReadFileToString(kControlPath, &buf, true)) {
+        LOG(ERROR) << "Failed to read " << kControlPath;
+        return false;
+    }
+
+    return std::stoi(android::base::Trim(buf)) == 1;
+}
+
+Return<bool> KeyDisabler::setEnabled(bool enabled) {
+    if (!mHasKeyDisabler) return false;
+
+    if (!android::base::WriteStringToFile((enabled ? "1" : "0"), kControlPath, true)) {
+        LOG(ERROR) << "Failed to write " << kControlPath;
+        return false;
+    }
+
+    return true;
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace touch
+}  // namespace lineage
+}  // namespace vendor

Thagoo/android10_device_xiaomi_rolex

Signed-off-by: Thagoo <lohitgowda56@gmail.com>

touch/KeyDisabler.cpp
@@ -1,0 +1,64 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <android-base/strings.h>
+
+#include "KeyDisabler.h"
+
+namespace vendor {
+namespace lineage {
+namespace touch {
+namespace V1_0 {
+namespace implementation {
+
+constexpr const char kControlPath[] = "/proc/touchpanel/capacitive_keys_disable";
+
+KeyDisabler::KeyDisabler() {
+    mHasKeyDisabler = !access(kControlPath, F_OK);
+}
+
+// Methods from ::vendor::lineage::touch::V1_0::IKeyDisabler follow.
+Return<bool> KeyDisabler::isEnabled() {
+    std::string buf;
+
+    if (!mHasKeyDisabler) return false;
+
+    if (!android::base::ReadFileToString(kControlPath, &buf, true)) {
+        LOG(ERROR) << "Failed to read " << kControlPath;
+        return false;
+    }
+
+    return std::stoi(android::base::Trim(buf)) == 1;
+}
+
+Return<bool> KeyDisabler::setEnabled(bool enabled) {
+    if (!mHasKeyDisabler) return false;
+
+    if (!android::base::WriteStringToFile((enabled ? "1" : "0"), kControlPath, true)) {
+        LOG(ERROR) << "Failed to write " << kControlPath;
+        return false;
+    }
+
+    return true;
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace touch
+}  // namespace lineage
+}  // namespace vendor